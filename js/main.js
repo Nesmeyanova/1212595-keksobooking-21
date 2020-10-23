f4bc72b9a7e7914bc27532687b1a017081382431
Kristina <k1tya@bk.ru>
2020-10-23 17:15:09 +0300

    с правками

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
-const TYPES = ['palace', 'flat', 'house', 'bungalow'];
-const TIME = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
-const getImg = function () {
-  for (let i = 1; i <= 8; i++) {
-    var s = '0' + 'i';
-    return s;
-  }
-}
+const PINS_COUNT = 8;
+const TYPES = [`palace`, `flat`, `house`, `bungalow`];
+const TIMES = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const URLS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const xCoordinates = {
+  MIN: 0,
+  MAX: 1200,
+};
+const yCoordinates = {
+  MIN: 130,
+  MAX: 630,
+};
 
 const getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const getRandomItem = function () { };
+const getRandomItem = function (items) {
+  var rand = Math.floor(Math.random() * items.length);
+    return items[rand];
+};
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
 
 const getRandomArray = (items) => {
-  function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-  }
-
-  for (let i = 0; i < 1000000; i++) {
-    let array = [1, 2, 3];
-    shuffle(array);
-    count[array.join('')]++;
-  }
-
+  shuffle(items)
   let clip = items.slice(0, getRandomInteger(1, items.length));
   return clip;
 }
@@ -33,38 +34,45 @@ const getRandomArray = (items) => {
 const generatePins = function () {
   const pins = [];
 
-for (let i = 1; i <= 8; i++) {
-  const pinItem = {
-    "author": {
-      // "avatar": img / avatars / user 'getImg' .png,
-    },
-    "offer": {
-      "title": 'Title of offer',
-      "address": "600, 350",
-      "price": getRandomInteger(1000, 10000),
-      "type": getRandomItem(TYPES),
-      "rooms": getRandomInteger(1, 3),
-      "guests": getRandomInteger(1, 5),
-      "checkin": getRandomItem(TIME),
-      "checkout": getRandomItem(TIME),
-      "features": getRandomArray(FEATURES),
-      "description": 'some description',
-      "photos": [
-        "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-        "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-        "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-      ]
-    },
-    "location": {
-      "x": getRandomInteger(0, 1200),
-      "y": getRandomInteger(130, 630).
-    }
-  };
-  pins.push(pinItem);
-}
-return pins;
+  for (let i = 1; i <= PINS_COUNT; i++) {
+    const pinItem = {
+      "author": {
+        "avatar": `img/avatars/user0${i}.png`,
+      },
+      "offer": {
+        "title": `Title of offer`,
+        "address": "600, 350",
+        "price": getRandomInteger(1000, 10000),
+        "type": getRandomItem(TYPES),
+        "rooms": getRandomInteger(1, 3),
+        "guests": getRandomInteger(1, 5),
+        "checkin": getRandomItem(TIMES),
+        "checkout": getRandomItem(TIMES),
+        "features": getRandomArray(FEATURES),
+        "description": `some description`,
+        "photos": getRandomArray(URLS),
+      },
+      "location": {
+        "x": getRandomInteger(xCoordinates.MIN, xCoordinates.MAX),
+        "y": getRandomInteger(yCoordinates.MIN, yCoordinates.MAX),
+      }
+    };
+    pins.push(pinItem);
+  }
+  return pins;
 };
 
-const pins = generatePins;
+const pins = generatePins();
+console.log(pins);
 
-// userDialog.classList.remove('.map--faded');
+document.querySelector('.map').classList.remove(`map--faded`);
+
+const fragment = document.createDocumentFragment();
+
+const similarPin = document.querySelector(`#pin`).content.querySelector(`button`);
+
+for (let i = 0; i < pins.length; i++) {
+  const newPin = similarPin.cloneNode(true);
+
+  fragment.appendChild(newPin);
+}
